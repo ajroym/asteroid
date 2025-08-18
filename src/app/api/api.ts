7490ca749c9fc4a2b7b85cabcc391a6b20835bdb
AJ <ajroym00@gmail.com>
2025-08-18 15:31:38 -0400

    Set up API requesting functionality

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,9 +1,9 @@
-const apiUrl = 'https://www.neowsapp.com/rest/v1'
+const API_URL = 'https://www.neowsapp.com/rest/v1';
 
 export const DateService = {
     getAsteroidByDate: function getAsteroidByDate(dateString: string) {
         const date = parseDate(dateString);
-        const url = apiUrl + `rest/v1/feed?start_date=${date}&end_date=${date}&detailed=true`
+        const url = API_URL + `rest/v1/feed?start_date=${date}&end_date=${date}&detailed=true`
         
         return requestData(url);
     } 
@@ -11,35 +11,45 @@ export const DateService = {
 
 export const NEOService = {
     getAsteroidById: function getAsteroidById(id: string) { 
-        const url = apiUrl + `/neo/${id}`;
-        return requestData(url);
+        const url = API_URL + `/neo/${id}`;
+        return requestData(url); 
     },
+    getSentryById: function getSentryById(id: string) {
+        const url = API_URL + `/neo/sentry/${id}`;
+        return requestData(url);
+    }
 }
 
 async function requestData(url: string) {
     const response = await fetch(url);
     try {
         if (!response.ok) {
-            return {
-                status: response.status,
-                statusText: response.statusText
-            };
+            throw new ClientRequestError(String(response.status), response.statusText)
         }
 
         const responseData = await response.json();
         return responseData;
 
     } catch (error) {
-
+        if (error instanceof ClientRequestError) {
+            return {
+                status: response.status,
+                statusText: response.statusText,
+                stackTrace: error.stack,
+            };
+        }
     }
 } 
-/*
-    TODO: Actually like. Do something in this function maybe.
-*/ 
-function parseDate(date: string) {
-    try {
-        return new Date(date);
-    } catch(error) {
 
+function parseDate(dateString: string) {
+    const date = new Date(dateString);
+    const currentDate = new Date();
+
+    if (date.getDay() < 0 || 
+    (date.getMonth() < 0 || date.getMonth() > 11) || 
+    (date.getFullYear() < 1900 || date.getFullYear() > currentDate.getFullYear())) {
+        throw new ApplicationError("Invalid date format entered (YYYY-MM-DD).", "Please enter a valid date.")
     }
+
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
