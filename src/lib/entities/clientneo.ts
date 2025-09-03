e0881236e379d540f9447f53effdb2b9f701c875
AJ <ajroym00@gmail.com>
2025-09-03 08:31:37 -0400

    Added streamlined NEO entity for displaying only desired data

---
+++ src/lib/entities/clientneo.ts
@@ -0,0 +1,10 @@
+export interface ClientNeo {
+    neoRefId: string,
+    name: string,
+    estimatedDiameterMeters: number,
+    missDistance: string,
+    closeApproachDate: string,
+    orbitingBody: string,
+    firstObservationDate: string,
+    lastObservationDate: string,
+}
