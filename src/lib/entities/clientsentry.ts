ec877484a79045994e710188d9aef9f46cdbfe5d
AJ <ajroym00@gmail.com>
2025-09-03 10:15:34 -0400

    Added sentry object for client side

---
+++ src/lib/entities/clientsentry.ts
@@ -0,0 +1,9 @@
+export interface ClientSentry {
+    spkId: string,
+    potentialImpacts: string,
+    impactProbability: string,
+    absoluteMagnitude: string,
+    estimatedDiameter: string,
+    torinoScale: string,
+    lastObservation: string
+}
