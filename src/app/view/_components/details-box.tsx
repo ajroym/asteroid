bbf2ae691868f758fb68c60e8c8a6c82649494c2
AJ <ajroym00@gmail.com>
2025-09-03 08:28:13 -0400

    Updated view to retrieve and  display object data.

--- src/app/view/_components/details-box.tsx
+++ src/app/view/_components/details-box.tsx
@@ -1,19 +1,32 @@
-export default function DetailsBox() {
+import { ClientNeo } from "@/lib/entities/clientneo";
+
+export default function DetailsBox({ clientNeo }: { clientNeo: ClientNeo }) {
     return (
         <div className="w-[80%] min-h-96">
             <div className="flex items-center justify-center gap-92 w-full my-8">
                 <div>
                     <label className="text-cyan-950 text-3xl font-semibold">Object Details</label>
                     <div className="w-96 h-128 bg-white rounded-md shadow-md shadow-neutral-400 p-6">
-                        <span className="text-black"><span className="font-semibold">test_key: </span>test_value</span>
-                        <div className="w-full h-[1px] bg-radial from-neutral-400 to-white"></div>
+                        <div className="flex flex-col gap-4">
+                        { Object.entries(clientNeo).map(([key, value]) => (
+                                <div key={key}>
+                                    <span className="text-black"><span className="font-semibold">{ key + ': ' } </span>{ value }</span>
+                                    <div className="w-full h-[1px] bg-radial from-neutral-400 to-white"></div>
+                                </div>
+                            
+                        ))}
+                        </div>
                     </div>
                 </div>
                 <div>
                     <label className="text-cyan-950 text-3xl font-semibold">Sentry Data</label>
                     <div className="w-96 h-128 bg-white rounded-md shadow-md shadow-neutral-400 p-6">
-                        <span className="text-black"><span className="font-semibold">test_key: </span>test_value</span>
-                        <div className="w-full h-[1px] bg-radial from-neutral-400 to-white"></div>
+                        { Object.entries(clientNeo).map(([key, value]) => (
+                            <div key={key}>
+                                <span className="text-black"><span className="font-semibold">{ key + ': ' } </span>{ value }</span>
+                                <div className="w-full h-[1px] bg-radial from-neutral-400 to-white"></div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -5,17 +5,60 @@ import Image from "next/image";
 
 import exampleImg from "../../../assets/example-img.jpg"
 import DetailsBox from "./_components/details-box";
+import { useEffect, useState } from "react";
+import { DateService } from "@/lib/api/api";
+import { NearEarthObject } from "@/lib/entities/neo";
+import { ClientNeo } from "@/lib/entities/clientneo";
 
 export default function ViewResults() {
+    const [neoClientObject, setNeoClientObject] = useState<ClientNeo>({
+        neoRefId: '0',
+        name: 'undefined_object',
+        estimatedDiameterMeters: 0.0,
+        missDistance: '0',
+        closeApproachDate: '01/01/1900',
+        orbitingBody: 'N/A',
+        firstObservationDate: '01/01/1900',
+        lastObservationDate: '01/01/1900',
+    })
+    const [riskLevel, setRiskLevel] = useState<string>('NONE');
+
     const queryParams = useSearchParams();
     const dateParam = queryParams.get('date');
+
+    const date = new Date(dateParam!);
+
+    const evaluateRiskLevel = (neo: NearEarthObject) => {
+        if (neo.is_potentially_hazardous_asteroid && !neo.is_sentry_object) {
+            setRiskLevel('LOW');
+        }
+    }
+
+    useEffect(() => {
+        const fetchObjectByDate = async () => {
+          DateService.getAsteroidByDate(dateParam!).then((response) => {
+            setNeoClientObject({
+                neoRefId: response!.neo_reference_id,
+                name: response!.name,
+                estimatedDiameterMeters: response!.estimated_diameter.meters.estimated_diameter_min,
+                missDistance: response!.close_approach_data[0].miss_distance.kilometers,
+                closeApproachDate: response!.close_approach_data[0].close_approach_date,
+                orbitingBody: response!.close_approach_data[0].orbiting_body,
+                firstObservationDate: response!.orbital_data.first_observation_date,
+                lastObservationDate: response!.orbital_data.last_observation_date,
+            })
+            evaluateRiskLevel(response!);
+          })
+        }
+        fetchObjectByDate();
+    }, [])
 
     return (
         <div className="w-full min-h-screen flex flex-col items-center px-8 gap-16">
             <div className="w-full flex flex-col items-center justify-center gap-12">
                 <div className="w-[90%]">
                     <div className="w-full text-cyan-950 text-3xl ml-2">
-                        Object Name, DD-MM-YYYY
+                        { neoClientObject.name }, { date.toLocaleDateString() }
                     </div>
                     <div className="w-full h-128">
                         <Image src={exampleImg} alt="example meteor image" className="w-full h-full border-[1px] border-neutral-400 rounded-lg object-cover object-top shadow-md shadow-neutral-500"></Image>
@@ -28,11 +71,14 @@ export default function ViewResults() {
                     <div className="text-cyan-950 text-4xl font-semibold text-shadow-lg">
                         RISK LEVEL:
                     </div>
-                    <div className="text-orange-500 text-5xl font-semibold text-shadow-lg/30">
-                        MEDIUM
+                    <div className={(riskLevel == 'NONE' ? 'text-cyan-500' : '') + 
+                                    (riskLevel == 'LOW' ? 'text-emerald-500' : '') +
+                                    (riskLevel == 'MEDIUM' ? 'text-orange-500' : '') + 
+                                    (riskLevel == 'HIGH' ? 'text-red-500' : '') + "  text-5xl font-semibold text-shadow-lg/30"}>
+                        { riskLevel }
                     </div>
                 </div>
-                <DetailsBox></DetailsBox>
+                <DetailsBox clientNeo={neoClientObject}></DetailsBox>
             </div>
         </div>
     )
